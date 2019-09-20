bb1f0123d5bf5baa498f8a1d24aaabb89457a03a
SB Components <gajender.singh@sb-components.co.uk>
2019-09-20 11:48:41 +0530

    Add files via upload

--- shims.d.ts
+++ shims.d.ts
@@ -4,20 +4,20 @@
 
     //% color=50 weight=19
     //% icon="\uf1eb"
-declare namespace Mbit_IR {
+declare namespace StemBit_IR {
 
     /**
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    //% block="on |%btn| button pressed" shim=StemBit_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    //% block="connect ir receiver to %pin" shim=StemBit_IR::init
     function init(pin: Pins): void;
 }
 
